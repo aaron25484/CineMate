a4dc3854ae48a5a9e70a8bcd15de924020cfe4ec
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-11-11 02:02:57 +0100

    fix: watchlist routes

--- SERVER/src/routes/user.routes.ts
+++ SERVER/src/routes/user.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { createUser, getAllUsers, deleteUser, updateUser, getUser } from "../controllers/user.controller";
+import { createUser, getAllUsers, deleteUser, updateUser, getUser, getUserWatchlist, deleteFromWatchlist } from "../controllers/user.controller";
 
 const userRoutes = Router()
 
 userRoutes.get('/', getAllUsers)
 userRoutes.post('/', createUser)
 userRoutes.delete('/:userId', deleteUser)
+userRoutes.delete('/:movieId', deleteFromWatchlist)
 userRoutes.get('/:userId', getUser)
+userRoutes.get('/userId/watchlist', getUserWatchlist)
 userRoutes.patch('/:userId', updateUser)
 
 export default userRoutes
