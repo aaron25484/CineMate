40aaf35462ffecbad20b923b99f9d8a4854c9199
Aaron de los Santos <aaronjournalist@hotmail.com>
2023-11-15 00:36:05 +0100

    fix: cors error

--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -3,9 +3,12 @@ import userRoutes from "./routes/user.routes";
 import genreRoutes from "./routes/genre.routes";
 import movieRoutes from "./routes/movie.routes";
 import prisma from "./db/client";
+import cors from "cors";
 
 const app: Application = express()
 app.use(express.json())
+
+app.use(cors())
 
 prisma.$connect().then(() => {
     console.log("Connected to MongoDB via Prisma");})
